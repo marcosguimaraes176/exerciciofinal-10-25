8dc66180749fea14261bffd0333db4003ad485d0
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-10 21:30:46 +0000

    final 31

--- pages/1.Abertura.py
+++ pages/1.Abertura.py
@@ -2,4 +2,13 @@ import streamlit as st
 
 
 st.image("fotos/maxresdefault-2.jpg")
-st.title("O Programa Música na Rede e sua influência no desempenho escolar")
+
+def titulo_com_tamanho(texto, tamanho=36, fonte='Elephant', negrito=True, cor='#191970'):
+    """Função para criar texto com tamanho personalizado"""
+    estilo_negrito = "font-weight: bold;" if negrito else ""
+    
+    st.markdown(f"<p style='font-size: {tamanho}px; font-family: {fonte}; color: {cor}; {estilo_negrito}'>{texto}</p>", 
+                unsafe_allow_html=True)
+
+titulo_com_tamanho("Programa Músicana Rede: características", tamanho=36, cor='#006666')
+#st.title("O Programa Música na Rede: Características")

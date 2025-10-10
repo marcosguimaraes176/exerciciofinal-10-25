7dbd4fb399465ac906956aac1c7ba4672094e71a
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-10 21:14:39 +0000

    final 30

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ import streamlit as st
 
 st.image("fotos/ifes.png")
 
-
-#def titulo_com_tamanho(texto, tamanho=32, fonte = 'Georgia', negrito=True cor='blue'):
-    #"""Função para criar texto com tamanho personalizado"""
-    #st.markdown(f"<p style='font-size: {tamanho}px; font-family: {fonte} color: {cor}; {estilo_negrito}'>{texto}</p>", 
-                #unsafe_allow_html=True)
     
-def titulo_com_tamanho(texto, tamanho=40, fonte='Georgia', negrito=True, cor='blue'):
+def titulo_com_tamanho(texto, tamanho=36, fonte='Georgia', negrito=True, cor='F5276C'):
     """Função para criar texto com tamanho personalizado"""
     estilo_negrito = "font-weight: bold;" if negrito else ""
     
@@ -16,17 +11,20 @@ def titulo_com_tamanho(texto, tamanho=40, fonte='Georgia', negrito=True, cor='bl
                 unsafe_allow_html=True)
 
 
-titulo_com_tamanho("Instituto Federal do Espírito Santo", tamanho=40, cor='red')
+titulo_com_tamanho("Instituto Federal do Espírito Santo", tamanho=36, cor='F5276C')
 # st.title("Instituto Federal do Espírito Santo - IFES") 
 
-def texto_com_tamanho(texto, tamanho=24, cor='blue'):
+def texto_com_tamanho(texto, tamanho=30, cor='F54927'):
     """Função para criar texto com tamanho personalizado"""
     st.markdown(f"<p style='font-size: {tamanho}px; color: {cor};'>{texto}</p>", 
                 unsafe_allow_html=True)
-texto_com_tamanho("Campus Serra", tamanho=24, cor='blue')
+texto_com_tamanho("Campus Serra", tamanho=30, cor='F54927')
 
-st.title("Disciplina:Cloud Computing para Produtos de Dados")
-st.text("Professor Maxwell Monteiro")
+
+titulo_com_tamanho("Disciplina:Cloud Computing para Produtos de Dados", tamanho=36, cor='#0000ff')
+texto_com_tamanho("Professor Maxwell Monteiro", tamanho=30, cor='#1e90ff')
+#st.title("Disciplina:Cloud Computing para Produtos de Dados")
+#st.text("Professor Maxwell Monteiro")
 
 st.title("Pesquisa: Programa Música na Rede")
 st.text("Aluno: Marcos Valerio Guimaraes")

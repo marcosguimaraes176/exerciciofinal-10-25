f81797f33cafc160aea8e83f2d85d7722ce1c0b9
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-10 21:47:23 +0000

    final 33

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import streamlit as st
 
-st.image("fotos/ifes.png")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("fotos/ifes.png", width=600)
 
     
-def titulo_com_tamanho(texto, tamanho=36, fonte='Georgia', negrito=True, cor='#191970'):
+def titulo_com_tamanho(texto, tamanho=36, fonte='Georgia', negrito=True, alinhamento = 'center', cor='#191970'):
     """Função para criar texto com tamanho personalizado"""
     estilo_negrito = "font-weight: bold;" if negrito else ""
     
-    st.markdown(f"<p style='font-size: {tamanho}px; font-family: {fonte}; color: {cor}; {estilo_negrito}'>{texto}</p>", 
+    st.markdown(f"<p style='font-size: {tamanho}px; font-family: {fonte}; color: {cor}; {estilo_negrito}; text-align: {alinhamento}'>{texto}</p>", 
                 unsafe_allow_html=True)
 
 
 titulo_com_tamanho("Instituto Federal do Espírito Santo", tamanho=36, cor='#191970')
 # st.title("Instituto Federal do Espírito Santo - IFES") 
 
-def texto_com_tamanho(texto, tamanho=30, fonte='Verdana', italico=True, cor='#191970'):
+def texto_com_tamanho(texto, tamanho=30, fonte='Verdana', italico=True, alinhamento = 'center1, cor='#191970'):
     """Função para criar texto com tamanho personalizado"""
     estilo_italico = "font-style: italic;" if italico else ""
-    st.markdown(f"<p style='font-size: {tamanho}px; ont-family: {fonte}; color: {cor}; {estilo_italico};'>{texto}</p>", 
+    st.markdown(f"<p style='font-size: {tamanho}px; ont-family: {fonte}; color: {cor}; {estilo_italico}; text-align: {alinhamento}'>{texto}</p>", 
                 unsafe_allow_html=True)
 texto_com_tamanho("Campus Serra", tamanho=30, cor='#191970')
 
--- pages/1.Abertura.py
+++ pages/1.Abertura.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-
-st.image("fotos/maxresdefault-2.jpg", width=400)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("fotos/maxresdefault-2.jpg", width=1200)
+#st.image("fotos/maxresdefault-2.jpg", width=1200)
 
 def titulo_com_tamanho(texto, tamanho=36, fonte='Bookman Old Style', negrito=True, alinhamento = 'center', cor='#191970'):
     """Função para criar texto com tamanho personalizado"""

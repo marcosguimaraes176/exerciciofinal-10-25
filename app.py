8bebd8294363b707d30dd1009f56a29953264c00
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-10 20:47:34 +0000

    final 27

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import streamlit as st
 st.image("fotos/ifes.png")
 
 
-def texto_com_tamanho(texto, tamanho=16, cor='blue'):
+def titulo_com_tamanho(texto, tamanho=16, cor='blue'):
     """Função para criar texto com tamanho personalizado"""
     st.markdown(f"<p style='font-size: {tamanho}px; color: {cor};'>{texto}</p>", 
                 unsafe_allow_html=True)
-st.title("Instituto Federal do Espírito Santo - IFES") 
+titulo_com_tamanho("Instituto Federal do Espírito Santo", tamanho=24, cor='red')
+# st.title("Instituto Federal do Espírito Santo - IFES") 
 
-
+def texto_com_tamanho(texto, tamanho=20, cor='blue'):
+    """Função para criar texto com tamanho personalizado"""
+    st.markdown(f"<p style='font-size: {tamanho}px; color: {cor};'>{texto}</p>", 
+                unsafe_allow_html=True)
 texto_com_tamanho("Campus Serra")
 
 st.title("Disciplina:Cloud Computing para Produtos de Dados")

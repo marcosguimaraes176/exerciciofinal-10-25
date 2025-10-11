0d64b82a941f67affd093d9e2b2b0441663e0688
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-11 01:09:27 +0000

    final 51

--- pages/4. metodologia.py
+++ pages/4. metodologia.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.title("Metodologia")
+st.title("Metodologia e Objetivos")
 
 st.text ("Pesquisa de campo para levantar dados de funcionamento do programa")
 st.text("Análise de dados fornecidos pela gestão do Programa Música na Rede")
 st.text ("Pesquisa em banco de dados do SEGES para avaliar desempenho escolar antes e depois da participação no Programa MnR")
+
+st.text("Objetivo: Desenvolver um aplicativo para registrar as presenças dos estudantes nos projetos e acompanhar o desempenho escolar.")

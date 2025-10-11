616ed007fba76687fde79bac77e6543e0b9ec02e
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-11 01:15:32 +0000

    final 52

--- pages/4. metodologia.py
+++ pages/4. metodologia.py
@@ -7,3 +7,5 @@ st.text("Análise de dados fornecidos pela gestão do Programa Música na Rede")
 st.text ("Pesquisa em banco de dados do SEGES para avaliar desempenho escolar antes e depois da participação no Programa MnR")
 
 st.text("Objetivo: Desenvolver um aplicativo para registrar as presenças dos estudantes nos projetos e acompanhar o desempenho escolar.")
+
+st.text("OBS: Os campos de acesso dos dados diários de presença e desempenho escolar estão em desenvolvimento e, por sua complexidade, não foi possível deixar funcional no prazo para análises dos colegas.")

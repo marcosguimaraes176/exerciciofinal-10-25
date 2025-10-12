1b3b6875fda7734a550a20b96e7b69b3ae97b8be
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-12 21:57:51 +0000

    final 57

--- pages/2. definicao.py
+++ pages/2. definicao.py
@@ -12,3 +12,9 @@ titulo_com_tamanho("Definição", tamanho=36, cor='#000080')
 st.text("Do tema: O Programa Música na Rede é uma iniciativa da SEDU-ES, FAPES e FAMES, que visa promover a educação musical nas escolas públicas do Espírito Santo. São ofertados quatro projetos: Bandas nas Escolas, Orquestra de Violões nas Escolas, Corais nas Escolas e Oruqestra Sinfônica Jovem nas Escolas")
 
 st.text("Do Objetivo da pesquisa: Investigar a influência do Programa Música na Rede no desempenho escolar dos estudantes da educação básica da Rede Estadual do Espírito Santo.")
+
+col1, col2, col3 = st.columns([1, 4, 1]) 
+
+with col2:
+    # A imagem é colocada na coluna 2 (a central), centralizando-a
+    st.image("fotos/MnR SantaJazz.jpeg", use_column_width=True)
